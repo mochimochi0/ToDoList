d54e2e62b126309eeb287c1b981d378c63ad4210
mochimochi0 <65543162+mochimochi0@users.noreply.github.com>
2020-07-19 17:26:58 +0900

    Create app.js

---
+++ app.js
@@ -0,0 +1,32 @@
+'use strict'
+
+const add = document.getElementById('list');
+
+document.getElementById('form').onsubmit = function(event){
+    event.preventDefault();
+    
+
+    const item = document.getElementById('form').word.value;
+
+const li = `<li class="d-flex list-group-item justify-content-between align-items-center mt-2">
+<span>${item}</span>
+<i class="fas fa-trash-restore-alt delete"></i>
+</li>`;
+
+add.insertAdjacentHTML('afterbegin',li)
+
+
+var reset=document.getElementById('input');
+reset.value = '';
+
+};
+
+
+// 削除機能
+
+list.addEventListener('click',function(e){
+  if(e.target.classList.contains('delete')){
+    e.target.parentElement.remove();
+  }
+})
+
