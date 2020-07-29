cb88aec47ee9048318c9a80071f8615900ad94af
mochimochi0 <65543162+mochimochi0@users.noreply.github.com>
2020-07-29 12:43:35 +0900

    Update app.js

--- app.js
+++ app.js
@@ -1,31 +1,31 @@
 'use strict'
-    const add = document.getElementById('list');
+const add = document.getElementById('list');
 
-    document.getElementById('form').onsubmit = function(event){
-        event.preventDefault();
-
-
-        const item = document.getElementById('form').word.value;
-
-    const li = `<li class="d-flex list-group-item justify-content-between align-items-center mt-2">
-    <span>${item}</span>
-    <i class="fas fa-trash-restore-alt delete"></i>
-    </li>`;
-
-    add.insertAdjacentHTML('afterbegin',li)
+document.getElementById('form').onsubmit = function(event){
+    event.preventDefault();
 
 
-    var reset=document.getElementById('input');
-    reset.value = '';
+    const item = document.getElementById('form').word.value;
 
-    };
+const li = `<li class="d-flex list-group-item justify-content-between align-items-center mt-2">
+<span>${item}</span>
+<i class="fas fa-trash-restore-alt delete"></i>
+</li>`;
+
+add.insertAdjacentHTML('afterbegin',li)
 
 
-    // 削除機能
+var reset=document.getElementById('input');
+reset.value = '';
 
-    list.addEventListener('click',function(e){
-      if(e.target.classList.contains('delete')){
-        e.target.parentElement.remove();
-      }
-    })
+};
+
+
+// 削除機能
+
+list.addEventListener('click',function(e){
+  if(e.target.classList.contains('delete')){
+    e.target.parentElement.remove();
+  }
+})
 
