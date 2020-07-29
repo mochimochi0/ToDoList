243637f5fb555015227b10d00c7213f1799fb329
mochimochi0 <65543162+mochimochi0@users.noreply.github.com>
2020-07-29 12:32:44 +0900

    Update app.js

--- app.js
+++ app.js
@@ -1,25 +1,25 @@
 'use strict'
+winidow.onload = function(){
+    const add = document.getElementById('list');
 
-const add = document.getElementById('list');
-
-document.getElementById('form').onsubmit = function(event){
-    event.preventDefault();
-    
-
-    const item = document.getElementById('form').word.value;
-
-const li = `<li class="d-flex list-group-item justify-content-between align-items-center mt-2">
-<span>${item}</span>
-<i class="fas fa-trash-restore-alt delete"></i>
-</li>`;
-
-add.insertAdjacentHTML('afterbegin',li)
+    document.getElementById('form').onsubmit = function(event){
+        event.preventDefault();
 
 
-var reset=document.getElementById('input');
-reset.value = '';
+        const item = document.getElementById('form').word.value;
 
-};
+    const li = `<li class="d-flex list-group-item justify-content-between align-items-center mt-2">
+    <span>${item}</span>
+    <i class="fas fa-trash-restore-alt delete"></i>
+    </li>`;
+
+    add.insertAdjacentHTML('afterbegin',li)
+
+
+    var reset=document.getElementById('input');
+    reset.value = '';
+
+    };
 
 
 // 削除機能
@@ -30,3 +30,4 @@ list.addEventListener('click',function(e){
   }
 })
 
+};
